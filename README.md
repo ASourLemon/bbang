eecbbbdd1a2e25876e9129262abd64c29f377f0c
Nelson Lopes Silva <nelsonflsilva@gmail.com>
2018-02-13 13:57:41 +0000

    Added termios test program. Updated readme

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 MCU Programming
 =======================================
 
-This repository for microcontroller programming experiences. 
+This is a repository for my microcontroller programming experiences. 
 
 For now, the goal is to implement a 16 bit, RS232-like, signal receiver.
 
 The target hardware is the Atmel ATmega328P.
+
+For testing, I used TERMIOS(3), 8Bit UART on a Raspberry Pi 3 Model B.
 
 
 References
@@ -13,3 +15,6 @@ References
 - [Arduino Duemilanove Schematics](https://www.arduino.cc/en/uploads/Main/arduino-duemilanove-schematic.pdf)
 - [ATmega168/328 Port Manipulation](https://www.arduino.cc/en/Reference/PortManipulation)
 - [ATmega168/328 Timers](https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328)
+- [Raspberry Pi gPIo] (https://docs.microsoft.com/en-us/windows/iot-core/media/pinmappingsrpi/rp2_pinout.png)
+- [Linux TERMIOS(3) Documentation] (http://man7.org/linux/man-pages/man3/termios.3.html)
+- [Raspberry Pi TERMIOS UART] (http://www.raspberry-projects.com/pi/programming-in-c/uart-serial-port/using-the-uart)
---
+++ bbang/termios_uart.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <unistd.h>			//Used for UART
+#include <fcntl.h>			//Used for UART
+#include <termios.h>		//Used for UART
+
+void setup();
+void receiveByte();
+void transmitByte();
+
+int fd = -1;
+
+int main() {
+	
+	setup();
+	
+	while(1) {
+		receiveByte();
+		sleep(1);
+	}
+
+	printf("Done!\n");
+	return 0;
+	
+}
+
+
+
+void setup() {
+	
+	const char *device = "/dev/ttyAMA0";
+	fd = open(device, O_RDWR | O_NOCTTY);	
+	if (fd == -1) { printf("Error - Unable to open UART.  Ensure it is not in use by another application\n"); }
+
+
+	struct termios options;
+	tcgetattr(fd, &options);
+	
+	options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+	
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &options);
+	
+}
+
+void receiveByte() {
+	
+	if (fd != -1)
+	{
+		unsigned char rx_buffer[2];
+		int rx_length = -1;
+		while(rx_length < 1) {
+			rx_length = read(fd, (void*)rx_buffer, 1);
+			if (rx_length < 0) { printf("Error!\n"); }
+			else if (rx_length == 0) { printf("No data waiting\n"); }
+			else
+			{
+				rx_buffer[rx_length] = '\0';
+				printf("%i bytes read : %s\n", rx_length, rx_buffer);
+				printf("\n");
+			}
+		}
+	}
+}
+
+void transmitByte() {
+	
+	unsigned char tx_buffer[20];
+	unsigned char *p_tx_buffer;
+	
+	p_tx_buffer = &tx_buffer[0];
+	*p_tx_buffer++ = 'a';
+	
+	if (fd != -1)
+	{
+		int count = write(fd, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
+		if (count < 0)
+		{
+			printf("UART TX error\n");
+		}
+	}
+	
+}
